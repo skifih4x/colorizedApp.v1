7eb637db2c08975c69da21eb30eee1a6241cd66a
artem <arte@kb.ri>
2022-02-12 23:38:03 +0300

    done code

--- colorizedApp.v1/ViewController.swift
+++ colorizedApp.v1/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redTextLabel: UILabel!
+    @IBOutlet var greenTextLabel: UILabel!
+    @IBOutlet var blueTextLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 10
+        
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        
+        let redSliderValue = CGFloat(redSlider.value)
+        let greenSliderValue = CGFloat(greenSlider.value)
+        let blueSliderValue = CGFloat(blueSlider.value)
+        
+        mainView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+        
+        redTextLabel.text = String(redSlider.value)
+        greenTextLabel.text = String(greenSlider.value)
+        blueTextLabel.text = String(blueSlider.value)
+        
+        redTextLabel.text = String(format: "%.2f", redSlider.value)
+        greenTextLabel.text = String(format: "%.2f", greenSlider.value)
+        blueTextLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
-
